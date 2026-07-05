28d2025d4697e7fce9568b94d90ce1e71b9f0d17
devrajpurohit050 <devrajpurohit050@gmail.com>
2026-07-05 14:58:48 +0530

    fix: resolve TypeScript production build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import CinematicSection from './components/CinematicSection'
